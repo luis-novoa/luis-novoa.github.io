5bef6105ed33b26a800ffa5887a2a51aeacc8ab3
Luis Novoa <luis.matte.novoa@gmail.com>
2020-04-17 15:35:25 -0400

    Finishes mobile layout

--- js/animations.js
+++ js/animations.js
@@ -1,10 +1,16 @@
 let mainPage = document.getElementById('main-page');
 let firstProject = document.getElementById('first-project');
 let secondProject = document.getElementById('second-project');
+let thirdProject = document.getElementById('third-project');
+let projectSection = document.getElementById('projects');
 let hideMainPage = document.getElementById('hide-main-page');
 let showMainPage = document.getElementById('show-main-page');
 let hideFirstProject = document.getElementById('hide-first-project');
 let showFirstProject = document.getElementById('show-first-project');
+let hideSecondProject = document.getElementById('hide-second-project');
+let showSecondProject = document.getElementById('show-second-project');
+let hideThirdProject = document.getElementById('hide-third-project');
+let showThirdProject = document.getElementById('show-third-project');
 
 hideMainPage.addEventListener('click', () => {
   mainPage.style.opacity = 0;
@@ -24,4 +30,24 @@ hideFirstProject.addEventListener('click', () => {
 showFirstProject.addEventListener('click', () => {
   firstProject.style.opacity = 1;
   firstProject.style.visibility = 'visible';
+});
+
+hideSecondProject.addEventListener('click', () => {
+  secondProject.style.opacity = 0;
+  secondProject.style.visibility = 'hidden';
+});
+
+showSecondProject.addEventListener('click', () => {
+  secondProject.style.opacity = 1;
+  secondProject.style.visibility = 'visible';
+});
+
+hideThirdProject.addEventListener('click', () => {
+  projectSection.style.opacity = 0;
+  projectSection.style.visibility = 'hidden';
+});
+
+showThirdProject.addEventListener('click', () => {
+  projectSection.style.opacity = 1;
+  projectSection.style.visibility = 'visible';
 });
