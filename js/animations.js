c5013d3fed05dd82bbf6ca1b157d63ac543ec6f7
Luis Novoa <luis.matte.novoa@gmail.com>
2020-04-17 11:23:46 -0400

    Adds transition between first and second projects

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,10 @@
 let mainPage = document.getElementById('main-page');
-let modifiedMainPage = window.getComputedStyle(mainPage);
+let firstProject = document.getElementById('first-project');
+let secondProject = document.getElementById('second-project');
 let hideMainPage = document.getElementById('hide-main-page');
 let showMainPage = document.getElementById('show-main-page');
+let hideFirstProject = document.getElementById('hide-first-project');
+let showFirstProject = document.getElementById('show-first-project');
 
 hideMainPage.addEventListener('click', () => {
   mainPage.style.opacity = 0;
@@ -11,4 +14,14 @@ hideMainPage.addEventListener('click', () => {
 showMainPage.addEventListener('click', () => {
   mainPage.style.opacity = 1;
   mainPage.style.visibility = 'visible';
+});
+
+hideFirstProject.addEventListener('click', () => {
+  firstProject.style.opacity = 0;
+  firstProject.style.visibility = 'hidden';
+});
+
+showFirstProject.addEventListener('click', () => {
+  firstProject.style.opacity = 1;
+  firstProject.style.visibility = 'visible';
 });
